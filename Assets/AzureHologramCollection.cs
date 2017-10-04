9d219bad95ad22d13877cb0f67d90f1871d50b2c
Pete D <peted70@hotmail.com>
2017-10-04 17:59:58 +0100

    removed the StorageServices submodule and refactored some interfaces into seperate code files

---
+++ Assets/AzureHologramCollection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity3dAzure.StorageServices;
+
+public class AzureHologramCollection : IHologramCollection
+{
+    private StorageServiceClient _storageClient;
+
+    public AzureHologramCollection()
+    {
+        var account = Environment.GetEnvironmentVariable("");
+        var accessKey = Environment.GetEnvironmentVariable("");
+
+        _storageClient = new StorageServiceClient(account, accessKey);
+    }
+
+    public Task<IEnumerable<IHologram>> GetHologramsAsync()
+    {
+        // enumerate the Azure Storage Container..
+        var blobs = _storageClient.GetBlobService();
+        StartCoroutine( blobs.ListBlobs()
+
+    }
+}
